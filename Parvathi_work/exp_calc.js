c991633b90517073974d30fcc8bd4f34011ec4a0
parlakmee3 <149263790+parlakmee3@users.noreply.github.com>
2025-01-26 08:28:07 -0600

    add the basic math function for calculating someone's remainder to pay others

---
+++ Parvathi_work/exp_calc.js
@@ -0,0 +1,20 @@
+const userTransactions = +10;
+const totalTransactions = 50;
+const totalUsers = 10;
+
+//equal distibution
+function calculateAmount (userTransactions, totalTransactions, totalUsers){
+    const toPay = userTransactions - totalTransactions/totalUsers; 
+
+    if (toPay < 0){
+        console.log("You owe", toPay);
+    }
+    else if (toPay > 0){
+        console.log("You are owed", toPay);
+    }
+    else {
+        console.log("Everything is perfect");
+    }
+}
+
+calculateAmount(userTransactions, totalTransactions, totalUsers);
